8566d2b4d358463b7a7fa37eea5385f3bd7c4438
ChaseLEngel <chaselengel@gmail.com>
2016-06-15 23:31:14 -0700

    First commit

---
+++ home_page.rb
@@ -0,0 +1,22 @@
+require_relative 'driver'
+require_relative 'sign_in_page'
+
+class HomePage
+  URL = 'https://vsco.co'
+
+  def initialize
+    @browser = Driver.instance.browser
+    if @browser.window.url == 'about:blank'
+      @browser.goto URL
+    end
+  end
+
+  def method_missing(method, *args, &block)
+    @browser.send(method, *args, &block)
+  end
+
+  def click_sign_in_link
+    @browser.link(text: 'Sign in').click
+    SignInPage.new
+  end
+end
